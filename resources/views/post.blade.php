83970411e5f7f8a80a9a620a3c9fc8e4ad35d023
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-12-05 12:14:12 +0100

    css

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -11,20 +11,30 @@
 
 <body>
     <h1>Welcome!</h1>
+    <div class="bento-position">
+    <article class="bento1">
+        <h2>First Post</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, magnam dolore atque libero id
+            perspiciatis fugit, illum, sit
+            ex temporibus non architecto asperiores recusandae hic maxime autem vitae esse odit.</p>
+    </article>
 
-    <h2>First Post</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, magnam dolore atque libero id
-        perspiciatis fugit, illum, sit
-        ex temporibus non architecto asperiores recusandae hic maxime autem vitae esse odit.</p>
-    <h2>Second Post</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, doloribus
-        ipsam delectus dolorem nam veritatis quidem iusto voluptatum necessitatibus repudiandae?</p>
-    <h2>Third Post</h2>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam obcaecati, ratione quisquam quam fuga aliquid
-        ipsum ut a, optio odit explicabo. Accusantium hic ipsa fugit dolorem possimus ullam dolores omnis praesentium
-        esse sequi minus eligendi iure neque enim repellat distinctio dignissimos asperiores ipsam, reprehenderit ut
-        porro soluta
-        perferendis? Ipsa adipisci, beatae hic voluptatum itaque a facere corporis alias expedita quidem.</p>
+    <article class="bento2">
+        <h2>Second Post</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, doloribus
+            ipsam delectus dolorem nam veritatis quidem iusto voluptatum necessitatibus repudiandae?</p>
+
+    </article>
+
+    <article class="bento3">
+        <h2>Third Post</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam obcaecati, ratione quisquam quam fuga aliquid
+            esse sequi minus eligendi iure neque enim repellat distinctio dignissimos asperiores ipsam, reprehenderit ut
+            porro soluta
+            perferendis? Ipsa adipisci, beatae hic voluptatum itaque a facere corporis alias expedita quidem.</p>
+    
+    </article>
+</div>
 </body>
 
 </html>
